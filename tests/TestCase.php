c98da239d5b6a4628146f031df64599ee71b74d9
David Ekechi <davemichael700@gmail.com>
2025-12-25 22:30:31 +0100

    chore: configure base test case and Pest helpers
    - Add RefreshDatabase trait to TestCase
    - Create custom Pest expectations (toBeModel, toHaveUlid)
    - Configure automatic test class usage for all tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    /**
+     * Indicates whether the default seeder should run before each test.
+     */
+    protected bool $seed = false;
 }
