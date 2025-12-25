1baeb6b14eeab69dd2778e90d236089bfc24d00e
David Ekechi <davemichael700@gmail.com>
2025-12-25 19:31:11 +0100

    chore: Ran php-cs-fixer

--- app/Modules/License/Enums/InstanceType.php
+++ app/Modules/License/Enums/InstanceType.php
@@ -6,18 +6,18 @@ namespace App\Modules\License\Enums;
 
 enum InstanceType: string
 {
-    case SITE = 'site';
+    case SITE   = 'site';
     case DEVICE = 'device';
     case SERVER = 'server';
 
     /**
      * Get all possible values
-     * 
+     *
      * @return array<string>
      */
     public static function values(): array
     {
-        return array_column(self::cases(), 'value');
+        return \array_column(self::cases(), 'value');
     }
 
     /**
@@ -26,7 +26,7 @@ enum InstanceType: string
     public function label(): string
     {
         return match ($this) {
-            self::SITE => 'Website',
+            self::SITE   => 'Website',
             self::DEVICE => 'Device',
             self::SERVER => 'Server',
         };
